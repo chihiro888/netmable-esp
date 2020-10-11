7b9542b0fc11c910c59cf8e252d784a0d9f29f2d
h4ppyy <93immm@naver.com>
2020-10-11 23:41:41 +0900

    알고리즘 푸는중

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
-
+#include <iostream>
+#include <list>
 
 // global variable
 LPCWSTR WindowsTitle = L"Netmable Overlay";
@@ -29,9 +30,48 @@ BYTE mask[12][22] = { 0 };
 // timer
 int idTimer = -1;
 
+// vector
+typedef struct _vector2D {
+    int x;
+    int y;
+} vector2D;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
+void initMaskArray() {
+    // 마스킹 배열 초기화
+    for (int x = 0; x < 12; x++) {
+        for (int y = 0; y < 22; y++) {
+            mask[x][y] = 0;
+        }
+    }
+}
+
+void drawOverlay(HWND hWnd) {
+    RECT  rect;
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hWnd, &ps);
+
+    HBRUSH myBrush = (HBRUSH)CreateSolidBrush(RGB(0, 0, 255));
+    HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, myBrush);
+
+    for (int x = 0; x < 12; x++) {
+        for (int y = 0; y < 22; y++) {
+            if (mask[x][y] == 11) {
+                rect.left = 155 + (y * 32);
+                rect.top = 125 + (x * 41);
+                rect.right = 190 + (y * 32);
+                rect.bottom = 160 + (x * 41);
+                Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
+            }
+        }
+    }
+
+    SelectObject(hdc, oldBrush);
+    DeleteObject(myBrush);
+
+    EndPaint(hWnd, &ps);
+}
 
 DWORD WINAPI SetWindowToTarget(LPVOID lpParam)
 {
@@ -70,14 +110,10 @@ DWORD WINAPI scanMemory(LPVOID lpParam) {
             }
         }
 
-        // 마스킹 배열 초기화
-        for (int x = 0; x < 12; x++) {
-            for (int y = 0; y < 22; y++) {
-                mask[x][y] = 0;
-            }
-        }
+        initMaskArray();
 
         // 이웃 가로축 마스킹
+        /*
         for (int x = 0; x < 12; x++) {
             for (int y = 0; y < 22; y++) {
                 if (y + 1 != 22 && result[x][y] != 0 && result[x][y] != 67 && result[x][y] == result[x][y + 1]) {
@@ -87,8 +123,10 @@ DWORD WINAPI scanMemory(LPVOID lpParam) {
                 }
             }
         }
+        */
 
         // 이웃 세로축 마스킹
+        /*
         for (int y = 0; y < 22; y++) {
             for (int x = 0; x < 12; x++) {
                 if (x + 1 != 12 && result[x][y] != 0 && result[x][y] != 67 && result[x][y] == result[x + 1][y]) {
@@ -98,37 +136,104 @@ DWORD WINAPI scanMemory(LPVOID lpParam) {
                 }
             }
         }
+        */
+
+        for (int y = 0; y < 22; y++) {
+            for (int x = 0; x < 12; x++) {
+                int origin = result[x][y];
+                for (int yy = 0; yy < 22; yy++) {
+                    for (int xx = 0; xx < 12; xx++) {
+                        if (x == xx && y == yy) {
+                            // pass
+                        } else {
+                            int clone = result[xx][yy];
+                            if (result[x][y] != 0 && result[x][y] != 67 && result[xx][yy] != 0 && result[xx][yy] != 67 && result[x][y] == result[xx][yy]) {
+                                // up (x y)
+                                for (int i = x - 1; i > -1; i--) {
+                                    if (result[i][y] == 0) {
+                                        mask[i][y] = 11;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                // up (xx yy)
+                                for (int i = xx - 1; i > -1; i--) {
+                                    if (result[i][yy] == 0) {
+                                        mask[i][yy] = 11;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                // down (x y)
+                                for (int i = x + 1; i < 12; i++) {
+                                    if (result[i][y] == 0) {
+                                        mask[i][y] = 11;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                // down (xx yy)
+                                for (int i = xx + 1; i < 12; i++) {
+                                    if (result[i][yy] == 0) {
+                                        mask[i][yy] = 11;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                // right (x y)
+                                for (int i = y+1; i < 22; i++) {
+                                    if (result[x][i] == 0) {
+                                        mask[x][i] = 11;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                // right (xx yy)
+                                for (int i = yy+1; i < 22; i++) {
+                                    if (result[xx][i] == 0) {
+                                        mask[xx][i] = 11;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                // left (x y)
+                                for (int i = y-1; i > -1; i--) {
+                                    if (result[x][i] == 0) {
+                                        mask[x][i] = 11;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                // left (xx yy)
+                                for (int i = yy-1; i > -1; i--) {
+                                    if (result[xx][i] == 0) {
+                                        mask[xx][i] = 11;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                mask[x][y] = 11;
+                                mask[xx][yy] = 11;
+                                goto EXIT;
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         EXIT:
             Sleep(100);
             InvalidateRect(hWnd, NULL, TRUE);
     }
-}
-
-void drawOverlay(HWND hWnd) {
-    RECT  rect;
-    PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(hWnd, &ps);
-
-    HBRUSH myBrush = (HBRUSH)CreateSolidBrush(RGB(0, 0, 255));
-    HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, myBrush);
-
-    for (int x = 0; x < 12; x++) {
-        for (int y = 0; y < 22; y++) {
-            if (mask[x][y] == 11) {
-                rect.left = 155 + (y * 32);
-                rect.top = 125 + (x * 41);
-                rect.right = 190 + (y * 32);
-                rect.bottom = 160 + (x * 41);
-                Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
-            }
-        }
-    }
-
-    SelectObject(hdc, oldBrush);
-    DeleteObject(myBrush);
-
-    EndPaint(hWnd, &ps);
 }
 
 INT APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
@@ -212,4 +317,3 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
     return 0;
 }
-
